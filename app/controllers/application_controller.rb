9cbe2a46d03276b2d027195e3150f485b0e19147
nakayama91 <k.naka10ry@gmail.com>
2024-09-10 21:06:23 +0900

    サインアップを社員番号に変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:employee_number, :password, :password_confirmation])
+  end
+
   def after_sign_in_path_for(resource)
     home_top_path
   end
 
-  private
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,28 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         authentication_keys: [:employee_number]
+
+  validates :employee_number, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+
+  # Use employee_number for login
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:employee_number)
+      where(conditions).where(employee_number: login).first
+    else
+      where(conditions).first
+    end
+  end
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
 end
+
